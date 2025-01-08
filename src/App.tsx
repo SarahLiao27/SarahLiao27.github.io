a1c7bcf0252a60e33810b246893465e72046ee83
Sarah Liao <sarah@Sarahs-MacBook-Pro.local>
2025-01-08 13:03:18 -0500

    feat: set up web pages + routing, made video carousel

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import "./styles/App.css";
+
+const App: React.FC = () => {
+  return (
+    <div>
+    <Router>
+      <div className="App">
+        <nav
+            className="navbar navbar-expand-lg navbar-custom"
+            aria-label="Main Navigation"
+        >
+            <div
+                className="container-fluid navbar-container"
+                aria-label="Navigation Container"
+            >
+                <a
+                    href="/"
+                    className="navbar-brand"
+                    aria-label="Sarah Liao's Website Home Page Link"
+                >
+                    ✧ Sarah Liao ✧ 
+                </a>
+                <div
+                    className="page-links"
+                    aria-label="Page Links"
+                >
+                    <a
+                        href="/about"
+                        className="about"
+                        aria-label="About Page Link"
+                    >
+                        About
+                    </a>
+                    <a
+                      href="/projects"
+                      className="projects"
+                      aria-label="Projects Page Link"
+                    >
+                      Projects
+                    </a>
+                    <a
+                      href="/resume"
+                      className="resume"
+                      aria-label="Resume Page Link"
+                    >
+                      Resume
+                    </a>
+                </div>
+            </div>
+        </nav>
+
+      <Routes>
+        <Route 
+            path="/" 
+            element={<HomePage aria-label="Home Page" />} />
+        <Route 
+            path="/about" 
+            element={<About aria-label="About Page" />} />
+        <Route 
+            path="/projects" 
+            element={<Projects aria-label="Projects Page" />} />
+        <Route 
+            path="/resume" 
+            element={<Resume aria-label="Resume Page" />} />
+      </Routes>
+      </div>
+
+    </Router>
+    </div>
+    
+  );
+};
+
+export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VideoCarousel from "../components/VideoCarousel";
+
+const HomePage = () => {
+    return (
+        <div>
+            <VideoCarousel/>
+        </div> 
+    );
+};
+
+export default HomePage;
