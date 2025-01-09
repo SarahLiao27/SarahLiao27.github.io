444dc1cad7407027fc909cbf1d16b2fa279c9861
Sarah Liao <sarah@Sarahs-MacBook-Pro.local>
2025-01-08 20:12:04 -0500

    feat: added background, added photo + intro blurb, todo: fix css of homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,12 +8,24 @@ const HomePage = () => {
             <h1>
                 Welcome!
             </h1>
-            <h4>
-                Each video has sound. Please adjust your volume before unmuting. 
-            </h4>
+            <div className="image-and-text-container">
+                <div className="image-container">
+                    <img src="src/components/images/Sarah.jpeg"/>
+                </div>
+                <div className="text-container">
+                    <h4>Hi! I'm Sarah, a current sophomore at Brown University studying computer science. I'm interested in software engineering and UI/UX. My hobbies include drawing, writing, and dancing (see the videos below)!</h4>
+                </div>
+            </div>
+            
             <div className="carousel-container">
                 <VideoCarousel/>
             </div>
+            <div className="video-text-container"> 
+                <h4>
+                    Each video has sound. Please adjust your volume before unmuting. 
+                </h4>
+            </div>
+            
         </div> 
     );
 };
