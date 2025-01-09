89ec3528757a099b47c84d197918f5054962c2c3
Sarah Liao <sarah@Sarahs-MacBook-Pro.local>
2025-01-09 11:10:13 -0500

    feat: added resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import "../styles/Resume.css";
 
 const Resume = () => {
     return (
-        <div>
-            <h1>resume page</h1> 
+        <div className="Resume">
+            {/* <h1>
+                Resume
+            </h1> */}
+            <a href="https://drive.google.com/file/d/1XobhGWOoBgJ0w1ifE5fXDEKJe7oufiNA/view?usp=sharing" target="_blank">
+                Resume
+            </a>
+            <iframe 
+                src="https://drive.google.com/file/d/1XobhGWOoBgJ0w1ifE5fXDEKJe7oufiNA/preview"
+            >
+            </iframe>
+            
         </div> 
     );
 };
 
 export default Resume;
+
+// https://drive.google.com/file/d/1XobhGWOoBgJ0w1ifE5fXDEKJe7oufiNA/view?usp=sharing
+
