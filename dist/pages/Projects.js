6ac64c1b7ebdea4d01ec3abbdc8d5769bf6becae
Sarah Liao <sarah@Sarahs-MacBook-Pro.local>
2025-01-13 13:57:24 -0500

    added deploy workflow

--- dist/pages/Projects.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Projects = () => {
-    return (_jsx("div", { children: _jsx("h1", { children: "projects page" }) }));
-};
-export default Projects;
--- dist/pages/Resume.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import "../styles/Resume.css";
-const Resume = () => {
-    return (_jsxs("div", Object.assign({ className: "Resume" }, { children: [_jsx("a", Object.assign({ href: "https://drive.google.com/file/d/1XobhGWOoBgJ0w1ifE5fXDEKJe7oufiNA/view?usp=sharing", target: "_blank" }, { children: "Resume" })), _jsx("iframe", { src: "https://drive.google.com/file/d/1XobhGWOoBgJ0w1ifE5fXDEKJe7oufiNA/preview" })] })));
-};
-export default Resume;
